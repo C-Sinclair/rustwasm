afb441244288b7ee25e531a8ec2785c422885f9f
Unknown <conor@wilxite.com>
2019-06-27 00:18:24 +0100

    save

--- web/index.js
+++ web/index.js
@@ -1,3 +1,6 @@
-import * as wasm from "wasm-game-of-life";
+import {
+    Universe
+} from "wasm-game-of-life";
 
-wasm.greet("Conor");
+const pre = document.getElementById('game-of-life-canvas')
+const universe = Universe.new();
